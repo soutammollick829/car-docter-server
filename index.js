ac0da793856d1be687ccbae8609cdaeb79ed31b9
soutammolick829 <soutammollick829@gmail.com>
2023-05-16 13:15:39 +0600

    services get-post-delete server api

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
   res.send("Docter is running");
 });
@@ -30,7 +29,7 @@ async function run() {
     await client.connect();
 
     const servicesCollection = client.db("carDocter").collection("services");
-    const bookingCollection = client.db("carDocter").collection("bookings")
+    const bookingCollection = client.db("carDocter").collection("bookings");
 
     app.get("/services", async (req, res) => {
       const cursor = servicesCollection.find();
@@ -38,24 +37,43 @@ async function run() {
       res.send(result);
     });
 
-
-    app.get('/services/:id', async(req , res)=>{
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
 
-      const result = await servicesCollection.findOne(query, options)
-      res.send(result)
-
-    })
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result);
+    });
 
     //booking services
-    app.post('/bookings', async(req,res)=>{
+
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
-    })
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
